813ed988e08a84843cfadee9a473d6f051674d07
Phelan Vendeville <phelan.vendeville@gmail.com>
2017-08-07 22:59:27 -0700

    Finish enumerable section

--- enumerable.rb
+++ enumerable.rb
@@ -60,8 +60,38 @@ end
 
 puts rot13(['Jul qvq gur puvpxra pebff gur ebnq?'])
 
-# Enumerable - reduce
-# Perform a single type of operation with all the elements and collect
-# the result (e.g., sum all the values in an array).
+# Enumerables: checking values within an object.
+def func_any(hash)
+    # Check and return true if any key object within the hash is of the type Integer
+    # If not found, return false.
+    return hash.any? {|key, value| key.is_a?(Integer)}
+end
 
+def func_all(hash)
+    # Check and return true if all the values within the hash are Integers and are < 10
+    # If not all values satisfy this, return false.
+    return hash.all? {|key, value| value.is_a?(Integer) && value < 10}
+end
+
+def func_none(hash)
+    # Check and return true if none of the values within the hash are nil
+    # If any value contains nil, return false.
+    return hash.none? {|key, value| value.nil?}
+end
+
+def func_find(hash)
+    # Check and return the first object that satisfies either of the following properties:
+    #   1. There is a [key, value] pair where the key and value are both Integers and the value is < 20 
+    #   2. There is a [key, value] pair where the key and value are both Strings and the value starts with `a`.
+    hash.find do |key, value|
+        (key.is_a?(Integer) and value.is_a?(Integer) and value < 20) or
+        (key.is_a?(String) and value.is_a?(String) and value.start_with?("a"))
+    end
+end
+
+# Enumerable: group_by
+def group_by_marks(marks, pass_marks)
+  # your code here
+    marks.group_by {|key, value| value >= pass_marks ? "Passed" : "Failed"}
+end
 
